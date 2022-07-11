0994cbc39e99ced7a064d75b6f1da4fdf7180cfc
Алла Седорина <kornilov.s@evrogas.local>
2022-07-11 14:27:07 +0300

    Добавил 13 задание

---
+++ Task_13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Ведите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int n=0;
+if(number>100 && number<1000)
+{n=number%10;}
+else if(number<10000 && number>1000)
+{
+    {n=number%100/10;}
+}
+Console.WriteLine("Третья цифра =" +n);
