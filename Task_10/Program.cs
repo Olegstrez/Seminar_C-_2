9b7a99c230200a646a0fd8353afa5f93056bebbd
Алла Седорина <kornilov.s@evrogas.local>
2022-07-11 13:54:33 +0300

    Задача 10

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -1,2 +1,10 @@
-﻿
-
+﻿Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number>100 && number<1000)
+{
+    Console.WriteLine("Вторая цифра ="+(number /10 % 10 ));
+} 
+else
+ {
+Console.WriteLine("Error");
+}
