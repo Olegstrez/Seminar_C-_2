1ec0793754564d2e7b3d558b53d0cc3dbaf9a082
Алла Седорина <kornilov.s@evrogas.local>
2022-07-11 14:35:14 +0300

    Добавил 15 задание

---
+++ Task_15/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number>7)
+{
+    Console.WriteLine("Error");
+} 
+else if(number>4&&number<8)
+ {
+Console.WriteLine("Да");
+}else
+ {
+Console.WriteLine("Нет");
+}
